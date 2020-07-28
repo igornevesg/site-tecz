05273b6387c67b5e88b748d5b6aab7365c609177
igornevesg <igornevesmoc@gmail.com>
2020-07-28 12:00:47 -0300

    começo botao responsivo

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,7 @@ function clickEmail() {
     //document.getElementById("formulario").style.display = "block";
     alert('Função indisponível no momento')
 }
+
+function abrirMenu() {
+    document.getElementById("myTopNav").style.display = "block";
+}
