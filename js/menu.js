50e6259f0b615ec3410a8e783d6b62ea6ce7ada7
igornevesg <igornevesmoc@gmail.com>
2020-09-08 17:24:34 -0300

    alteracoes responsividade

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,23 @@
 function clickEmail() {
-    //document.getElementById("formulario").style.display = "block";
-    alert('Função indisponível no momento')
+    var email = document.getElementById("formulario");
+    if (email.style.display == "none") {
+        email.style.display = "block";
+    } else {
+        email.style.display = "none"
+    }
 }
 
 function myFunction(x) {
     if (x.matches) {
-      document.getElementById("myTopNav").style.display = 'block';
+        document.getElementById("myTopNav").style.display = 'block';
     } else {
-        document.getElementById("myTopNav").style.display = 'none';  
+        document.getElementById("myTopNav").style.display = 'none';
     }
-  }
-  
-  var x = window.matchMedia("(min-width: 769px)")
-  myFunction(x);
-  x.addListener(myFunction);
+}
+
+var x = window.matchMedia("(min-width: 769px)")
+myFunction(x);
+x.addListener(myFunction);
 
 function abrirMenu() {
     var submenu = document.getElementById("myTopNav");
@@ -21,5 +25,5 @@ function abrirMenu() {
         submenu.style.display = 'block';
     } else {
         submenu.style.display = 'none';
-    } 
+    }
 }
